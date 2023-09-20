d8b6cab8a66a7a15397ace8cde9228a36b4bd2a9
intmax <563748846@qq.com>
2023-09-20 11:40:02 -0600

    modify base

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -2,7 +2,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/",
+  base: "/cmput401-docs-template/", // TODO
 
   lang: "en-US",
   title: "Docs Demo",
